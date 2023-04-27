a72c2bf9b500ebe757685ae22570463e721dd62d
Ved <vedankpachkawade@gmail.com>
2023-04-28 04:54:23 +0530

    Initial commit

--- app/src/main/java/com/example/fatalframes/HomeActivity.java
+++ app/src/main/java/com/example/fatalframes/HomeActivity.java
@@ -2,32 +2,42 @@ package com.example.fatalframes;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.example.fatalframes.Model.Product;
+import com.example.fatalframes.Prevalent.Prevalent;
+import com.example.fatalframes.ViewHome.ProductViewHolder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
-
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import io.paperdb.Paper;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private android.content.Intent Intent;
+    private DatabaseReference ProductsRef;
+
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ProductsRef = FirebaseDatabase.getInstance().getReference().child("Products");
 
         Paper.init(this);
 
@@ -48,9 +58,36 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View headerView = navigationView.getHeaderView(0);
+        TextView userNameTextView = headerView.findViewById(R.id.user_profile_name);
+        CircleImageView profileImageView = headerView.findViewById(R.id.user_profile_image);
+        userNameTextView.setText(Prevalent.currentOnlineUser.getName());
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerOptions<Product> options = new FirebaseRecyclerOptions.Builder<Product>().setQuery(ProductsRef, Product.class).build();
+
+        FirebaseRecycleAdapter<Products, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<Products, ProductViewHolder>(){
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder holder,int position,@NonNull Product){
+                holder.txtProductionName.setText(model.getPname());
+                holder.txtProductDescription.setText(model.getPname());
+                holder.txtProductprice.setText(model.getPname());
+            }
+            @NonNull
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.id.product_items_laout, parent, false);
+                ProductViewHolder holder = new ProductViewHolder(View);
+                return holder;
+            }
+        }
+    }
 
     public void onBlackPressed(){
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
